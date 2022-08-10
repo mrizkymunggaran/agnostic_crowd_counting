5330118f7d4b59077795be57ae2d822d46185cb6
rizky.munggaran <rizky.munggaran@nodeflux.io>
2022-08-10 16:53:01 +0700

    sfs

--- run.sh
+++ run.sh
@@ -5,8 +5,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 apt install docker-ce
-usermod -aG docker ${USER}
-su - ${USER}
 
 docker network create agnostic-counting
 
