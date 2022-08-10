fb14b8c2541d46076e9172cf9884b92258847164
rizky.munggaran <rizky.munggaran@nodeflux.io>
2022-08-10 16:37:31 +0700

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,19 @@
 
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+#!/bin/bash
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common 
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add - 
+sudo add-apt-repository \
+    "deb https://apt.dockerproject.org/repo/ \
+    ubuntu-$(lsb_release -cs) \
+    main" 
+sudo apt-get update
+sudo apt-get -y install docker-engine 
+# add current user to docker group so there is no need to use sudo when running docker
+sudo usermod -aG docker $(whoami)
 
 docker network create agnostic-counting
 
